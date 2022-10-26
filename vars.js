320ba5717c46b9ad8b1b46831f714cc8fd6f25dd
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2022-10-26 21:40:59 +0100

    Initial commit

---
+++ vars.js
@@ -0,0 +1,23 @@
+const strings = {
+    captions: {
+        desktop: "(Click to generate new excuse)",
+        mobile: "(Tap to generate new excuse)"
+    }
+}
+
+const elements = {
+    button_element: document.getElementById("wrapper"),
+    excuse_element: document.getElementById("excuse"),
+    caption_element: document.getElementById("caption"),
+
+    box_element: document.getElementById("box"),
+
+    stats_wrapper: document.getElementById("stats_box"),
+    dimmer_element: document.getElementById("stats_wrapper"),
+    stats_element: document.getElementById("stats")
+
+}
+
+const vars = {
+    code: "Space"
+}
